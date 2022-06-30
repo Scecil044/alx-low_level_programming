7d259438f164f7578ea42201a2527554bf58b056
Scecil044 <scecil072@gmail.com>
2022-06-30 13:19:10 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main - to be called
+ *
+ *Return: 0 for int
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
