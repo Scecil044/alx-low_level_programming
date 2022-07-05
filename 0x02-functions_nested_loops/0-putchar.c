38d8b6f6571a609346b64bc8bf757ac2a672e9b1
Scecil044 <scecil072@gmail.com>
2022-07-05 10:50:40 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - start of code
+ *
+ * Return: always zero
+*/
+int main(void)
+{
+char sync[] = "sync";
+int i;
+for (i = 0; sync[i] != '\0'; i++)
+{
+_putchar(sync[i]);
+}
+_putchar('\n');
+return (0);
+}
