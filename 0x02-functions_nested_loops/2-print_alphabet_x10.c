a377acbcaa8945c4ff69083b8fc3761f31c14bb9
Scecil044 <scecil072@gmail.com>
2022-07-05 06:00:54 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ *
+ * Return: Always 0
+*/
+
+void print_alphabet_x10(void)
+{
+char letra;
+char retorno = '\n';
+int i;
+for (i = 0; i < 10 ; i++)
+{
+letra = 'a';
+while (letra <= 'z')
+{
+_putchar (letra);
+letra++;
+}
+_putchar (retorno);
+}
+}
